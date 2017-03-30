817e9615bc0491842320a2a31f6ea86c7f294253
jcanny <jfc@cs.berkeley.edu>
2017-03-30 12:18:46 -0700

    update to v2.0

--- src/main/scala/BIDMach/rl/ALE.scala
+++ src/main/scala/BIDMach/rl/ALE.scala
@@ -1,5 +1,5 @@
 package BIDMach.rl;
-import BIDMat.{IMat,FMat,FND};
+import BIDMat.{IMat,FMat};
 import BIDMat.MatFunctions._;
 import java.util.Random;
 
@@ -9,7 +9,7 @@ class ALE extends edu.berkeley.bid.ALE {
   val rg = new Random();
   var frameskip = (2, 5);
 
-  def copyObs(out0:FND):FND = {
+  def copyObs(out0:FMat):FMat = {
 		if (dims == null) {
 			dims = getScreenDims
 		}
@@ -17,7 +17,7 @@ class ALE extends edu.berkeley.bid.ALE {
 		val width = dims(0);
 		val height = dims(1);
 		val len = width*height;
-		val out = if (out0.asInstanceOf[AnyRef] == null) FND(3, width, height) else out0;
+		val out = if (out0.asInstanceOf[AnyRef] == null) zeros(3 \ width \ height) else out0;
 		val odata = out.data;
 		var i = 0;
 		while (i < len*3) {
@@ -27,9 +27,9 @@ class ALE extends edu.berkeley.bid.ALE {
 		out;
   }
   
-  def copyObs():FND = copyObs(null);
+  def copyObs():FMat = copyObs(null);
 
-  def step(action:Int, out0:FND):(FND, Float, Boolean) = {
+  def step(action:Int, out0:FMat):(FMat, Float, Boolean) = {
   	val nsteps = frameskip._1 + rg.nextInt(frameskip._2 - frameskip._1 + 1);
   	var reward = 0f;
   	var i = 0;
@@ -48,14 +48,14 @@ class ALE extends edu.berkeley.bid.ALE {
   	(out, reward, done)
   };
   
-  def step(action:Int):(FND, Float, Boolean) = step(action, null);
+  def step(action:Int):(FMat, Float, Boolean) = step(action, null);
 
-  def reset(out0:FND):FND = {
+  def reset(out0:FMat):FMat = {
 		reset_game();
 		copyObs(out0);
   };
   
-  def reset():FND = reset(null);
+  def reset():FMat = reset(null);
 
   override def loadROM(s:String):Int = {
   		val status = super.loadROM(s);
@@ -68,9 +68,9 @@ class ALE extends edu.berkeley.bid.ALE {
 object ALE {
 	val rg = new Random();
 
-	def stepAll(envs:Array[ALE],  actions:IMat, obs0:Array[FND], rewards0:FMat, dones0:FMat):(Array[FND], FMat, FMat) = {
+	def stepAll(envs:Array[ALE],  actions:IMat, obs0:Array[FMat], rewards0:FMat, dones0:FMat):(Array[FMat], FMat, FMat) = {
 		val npar = envs.length;
-		val obs = if (obs0.asInstanceOf[AnyRef] == null) new Array[FND](npar) else obs0;
+		val obs = if (obs0.asInstanceOf[AnyRef] == null) new Array[FMat](npar) else obs0;
 		val rewards = if (rewards0.asInstanceOf[AnyRef] == null) zeros(1, npar) else rewards0;
 		val dones = if (dones0.asInstanceOf[AnyRef] == null) zeros(1, npar) else dones0;
 		(0 until npar).par.foreach((i) => {
@@ -83,7 +83,7 @@ object ALE {
 		(obs, rewards, dones)
 	};
 	
-	def stepAll(envs:Array[ALE], actions:IMat):(Array[FND], FMat, FMat) = stepAll(envs, actions, null, null, null);
+	def stepAll(envs:Array[ALE], actions:IMat):(Array[FMat], FMat, FMat) = stepAll(envs, actions, null, null, null);
 
 	def stepAll2(envs:Array[ALE], actions:IMat, obs0:Array[Array[Byte]], rewards0:FMat, dones0:FMat):(Array[Array[Byte]], FMat, FMat) = {
 		val npar = envs.length;
